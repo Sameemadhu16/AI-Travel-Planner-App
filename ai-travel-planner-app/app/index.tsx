c649c37093eb69a4e693d9bea91ba027ecdcb8f5
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-08-31 08:08:32 +0530

    set up the project

---
+++ ai-travel-planner-app/app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        
+      }}
+    >
+      <Text>Subscribe to samiya16!</Text>
+    </View>
+  );
+}
